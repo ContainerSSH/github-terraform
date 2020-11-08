f8841a2d44ed1d9e7900c5637abd263bba61c057
Janos Pasztor <janos@pasztor.at>
2020-11-08 18:16:02 +0100

    Added sshserver-auditlog repo

--- variables.tf
+++ variables.tf
@@ -14,6 +14,7 @@ locals {
   libraries = {
     "github-terraform": "Terraform repository for managing this GitHub organization"
     "sshserver": "The SSH server and decoding library used by ContainerSSH",
+    "sshserver-auditlog": "Auditlog integration for the SSH server library"
     "log": "Common logging interface for ContainerSSH modules",
     "auditlog": "Audit logger for ContainerSSH",
   }
