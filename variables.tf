78b852be9c5db0caf544bab1f01639af0ffb61b4
Janos Pasztor <janoszen@users.noreply.github.com>
2020-11-13 09:55:50 +0100

    authintegration

--- variables.tf
+++ variables.tf
@@ -14,6 +14,7 @@ locals {
   libraries = {
     "http": "Common HTTP library for ContainerSSH",
     "auth": "ContainerSSH authentication library",
+    "authintegration": "SSH server integration of the auth library"
     "github-terraform": "Terraform repository for managing this GitHub organization"
     "sshserver": "The SSH server and decoding library used by ContainerSSH",
     "auditlogintegration": "Auditlog integration for the SSH server library"
