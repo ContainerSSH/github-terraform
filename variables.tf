5297b53951c831d01c0d60aa64bf5cf7ab1a8239
Janos Pasztor <janoszen@users.noreply.github.com>
2020-11-17 22:46:29 +0100

    Added necessary libraries for ContainerSSH refactor

--- variables.tf
+++ variables.tf
@@ -12,6 +12,12 @@ locals {
     "sanjabonic"
   ]
   libraries = {
+    "geoip": "The GeoIP lookup library for ContainerSSH",
+    "logintegration:" "The logging integration library for ContainerSSH",
+    "dockerrun": "The Docker backend for ContainerSSH",
+    "kuberun:" "The Kubernetes backend for ContainerSSH",
+    "backend": "Container backend abstraction library for ContainerSSH",
+    "metrics": "Metrics collection and server library for ContainerSSH",
     "configuration": "Common configuration library for ContainerSSH",
     "http": "Common HTTP library for ContainerSSH",
     "auth": "ContainerSSH authentication library",
