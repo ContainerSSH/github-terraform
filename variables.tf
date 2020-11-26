f8b27efdbebbf6190c19f5ce1e838ba0ceaebd3a
Janos Pasztor <janos@pasztor.at>
2020-11-26 22:57:30 +0100

    Added sshserverintegration

--- variables.tf
+++ variables.tf
@@ -26,6 +26,7 @@ locals {
     "authintegration": "SSH server integration of the auth library"
     "github-terraform": "Terraform repository for managing this GitHub organization"
     "sshserver": "The SSH server and decoding library used by ContainerSSH",
+    "sshserverintegration": "Integration library for the SSH server library for ContainerSSH"
     "auditlogintegration": "Auditlog integration for the SSH server library"
     "log": "Common logging interface for ContainerSSH modules",
     "auditlog": "Audit logger for ContainerSSH",
