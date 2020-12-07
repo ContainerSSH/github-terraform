5a60545d145ec5a7ef4574f41f15dd5a6a9d36e2
Janos Pasztor <janos@pasztor.at>
2020-12-07 18:00:37 +0100

    Added AuthConfig server repo

---
+++ authconfig.tf
@@ -0,0 +1,51 @@
+resource "github_repository" "authconfig" {
+  name = "AuthConfig"
+  description = "The Authentication and Configuration Server for ContainerSSH"
+  has_issues = true
+  has_projects = false
+  has_wiki = false
+  has_downloads = true
+  vulnerability_alerts = true
+  default_branch = "main"
+  homepage_url = "https://containerssh.github.io/"
+
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = false
+
+  topics = [
+    "docker",
+    "kubernetes",
+    "ssh",
+    "devsecops",
+    "security",
+  ]
+  template {
+    owner      = "ContainerSSH"
+    repository = "library-template"
+  }
+  lifecycle {
+    ignore_changes = [
+      template,
+      has_downloads,
+      topics,
+    ]
+    prevent_destroy = true
+  }
+}
+
+//noinspection MissingProperty
+resource "github_branch_protection" "authconfig" {
+  repository_id          = github_repository.authconfig.node_id
+  pattern                = "main"
+  enforce_admins         = true
+  require_signed_commits = true
+  required_status_checks {
+    strict = true
+    contexts = [
+      "CodeQL",
+      "Run lint",
+      "Run tests",
+    ]
+  }
+}
