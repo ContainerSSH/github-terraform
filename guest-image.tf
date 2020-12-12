59c7452fe6d2e4a25a78140cb294aca0dcc9e0ff
Janos Pasztor <janos@pasztor.at>
2020-12-12 23:06:08 +0100

    Added repository for ContainerSSH guest image

---
+++ guest-image.tf
@@ -0,0 +1,54 @@
+resource "github_repository" "guest-image" {
+  name                 = "guest-image"
+  description          = "The source code of the default ContainerSSH guest image"
+  has_issues           = true
+  has_projects         = false
+  has_wiki             = false
+  has_downloads        = true
+  vulnerability_alerts = true
+  default_branch       = "main"
+  homepage_url         = "https://containerssh.io/"
+
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = false
+
+  delete_branch_on_merge = true
+
+  topics = [
+    "docker",
+    "kubernetes",
+    "ssh",
+    "devsecops",
+    "security",
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      template,
+      has_downloads,
+      topics,
+    ]
+    prevent_destroy = true
+  }
+}
+
+//noinspection MissingProperty
+resource "github_branch_protection" "guest-image" {
+  repository_id          = github_repository.guest-image.node_id
+  pattern                = "main"
+  enforce_admins         = false
+  require_signed_commits = true
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    required_approving_review_count = 1
+  }
+  required_status_checks {
+    strict = true
+    contexts = [
+      "CodeQL",
+      "Run lint",
+      "Run tests",
+    ]
+  }
+}
