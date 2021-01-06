c99095a2c6c325f84406f3db6d9bbefa78d6ab9f
Janos Pasztor <janos@pasztor.at>
2021-01-06 13:22:59 +0100

    Added packages repository

---
+++ packages.tf
@@ -0,0 +1,50 @@
+resource "github_repository" "packages" {
+  name                 = "packages"
+  description          = "The ContainerSSH packages page"
+  has_issues           = true
+  has_projects         = false
+  has_wiki             = false
+  has_downloads        = true
+  vulnerability_alerts = true
+  default_branch       = "main"
+  homepage_url         = "https://containerssh.io/"
+
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = false
+
+  delete_branch_on_merge = true
+
+  topics = [
+    "docker",
+    "kubernetes",
+    "ssh",
+    "devsecops",
+    "security",
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      template,
+      has_downloads,
+      topics,
+    ]
+    prevent_destroy = true
+  }
+}
+
+//noinspection MissingProperty
+resource "github_branch_protection" "packages" {
+  repository_id          = github_repository.packages.node_id
+  pattern                = "main"
+  enforce_admins         = false
+  require_signed_commits = true
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    required_approving_review_count = 1
+  }
+  required_status_checks {
+    strict   = true
+    contexts = []
+  }
+}
