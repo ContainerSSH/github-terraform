7522e997d5f05eae69b8a12fcff340ae22e35e22
Janos Bonic <86970079+janosdebugs@users.noreply.github.com>
2022-07-26 06:37:54 +0200

    Added Helm charts repository

---
+++ charts.tf
@@ -0,0 +1,61 @@
+resource "github_repository" "charts" {
+  name                 = "charts"
+  description          = "The ContainerSSH Helm charts"
+  has_issues           = true
+  has_projects         = false
+  has_wiki             = false
+  has_downloads        = false
+  vulnerability_alerts = true
+  default_branch       = "main"
+  homepage_url         = "https://containerssh.io/"
+
+  allow_merge_commit = false
+  allow_squash_merge = true
+  allow_rebase_merge = true
+  allow_auto_merge   = true
+
+  delete_branch_on_merge = true
+
+  topics = [
+    "docker",
+    "kubernetes",
+    "ssh",
+    "devsecops",
+    "security",
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      template,
+      has_downloads,
+      topics,
+    ]
+    prevent_destroy = true
+  }
+}
+
+resource "github_team_repository" "charts" {
+  repository = github_repository.charts.name
+  team_id    = github_team.chairs.id
+  permission = "admin"
+}
+
+//noinspection MissingProperty
+resource "github_branch_protection" "charts" {
+  repository_id          = github_repository.charts.node_id
+  pattern                = "main"
+  enforce_admins         = false
+  require_signed_commits = false
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    required_approving_review_count = 1
+  }
+  required_status_checks {
+    strict   = true
+    contexts = []
+  }
+  push_restrictions = [
+    github_team.chairs.node_id,
+    github_team.bots.node_id,
+  ]
+}
